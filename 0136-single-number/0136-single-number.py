7e0f184c69a8f93bb9efa9dce6924059c6432f59
Gabriel Montrone <114433572+gabrielmontrone@users.noreply.github.com>
2024-07-30 15:46:04 -0300

    Time: 108 ms (67.83%), Space: 19 MB (48.74%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num # Uso de XOR
+        return result
+                
